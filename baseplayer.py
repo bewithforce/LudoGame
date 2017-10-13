cc6d522f8d8a39ca4dccc4371b46414d5c1e1324
Štěpán Balážik <sbalazik@gmail.com>
2017-10-13 04:15:00 +0200

    Refactoring

---
+++ baseplayer.py
@@ -0,0 +1,71 @@
+import clobrdo
+
+
+class BasePlayer:
+    def __init__(self, name=None):
+        self.name = name
+        self.start = None
+        self.finish = []
+        self.new_game = True
+        self._game = None
+        self.moves_stalled = 0
+
+    @property
+    def n_pieces(self):
+        return self._game.n_pieces
+
+    def strategy(self):
+        """One call of function should result in one move on the board if such move is possible"""
+        pass
+
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def die(self):
+        return self._game.die
+
+    @property
+    def n_die(self):
+        return self._game.n_die
+
+    def move(self, field_from):
+        try:
+            self._game.move(self, field_from)
+            return True
+        except clobrdo.IllegalMove:
+            return False
+
+    def check(self, field):
+        return self._game.board.get(self.unshift_field(field))
+
+    def pieces(self, target_player=None):
+        """Returns list of all pieces in play belonging to a target_player (if not specified return the pieces of player
+        asking"""
+        # TODO: Should players see to finishes of others?? Hard to implement in current state.
+        if target_player is None:
+            target_player = self
+        pieces = [self.shift_field(item[0]) for item in self._game.board.items() if item[1] == target_player]
+        if target_player == self:
+            pieces += [self._game.board_size + i for i, x in enumerate(self.finish) if x]
+        return pieces
+
+    def is_in_danger(self, field):
+        for f in range(field - 1, field - self.n_die, -1):
+            if self.check(f) not in [None, self]:
+                return True
+        return False
+
+    def enemies(self):
+        return [player for player in self._game.players if player != self]
+
+    def shift_field(self, field):
+        """Player class wraps the playing board in such a way that it is seen by the strategy as indexed in terms of
+        its on start and finish. This is done using shift_field and unshift_field functions."""
+        return (field - self.start) % self._game.board_size
+
+    def unshift_field(self, field):
+        return (field + self.start) % self._game.board_size
--- clobrdo.py
+++ clobrdo.py
@@ -1,27 +1,32 @@
 from random import randint, shuffle
 
-
 START = -1
 
 
 class IllegalMove(Exception):
-    def __init__(self, message, player, board):
+    def __init__(self, message, player, game):
         self.player = player
-        self.board = board
-        super().__init__(message)
+        self.board = game.board
+        super().__init__(message, self.player, self.board)
 
 
 class DidNotMove(IllegalMove):
     pass
 
 
+class MovedMoreThanOnce(IllegalMove):
+    pass
+
+
 class Clobrdo:
     """Main class, holds the game state, checks all the rules."""
+
     def __init__(self, players, n_pieces=4, length=10, n_die=6):
         # Initialize players
         for i, player in enumerate(players):
             player.start = i * length
-            player._board = self
+            player._game = self
+            player.new_game = True
             player.finish = [False] * n_pieces
         shuffle(players)
         self.players = players
@@ -30,13 +35,14 @@ class Clobrdo:
         self.board = {}
         self.board_size = length * len(players)
         self.n_pieces = n_pieces
+        self.full_house = [True] * self.n_pieces
 
         # Initialize die
         self.n_die = n_die
         self.die = None
 
         # State variables
-        self.moved = False
+        self.moved = 0
         self.rank = []
         self.n_moves = 0
 
@@ -44,10 +50,9 @@ class Clobrdo:
         """Main loop of the game, """
         while self.players:
             for player in self.players:
+                if self.n_moves > 1000:
+                    pass
                 self.player_move(player)
-                if self.die == 6:
-                    self.player_move(player)
-                    player.new_game = False
         return self.rank
 
     def player_move(self, player):
@@ -55,7 +60,7 @@ class Clobrdo:
         if player in self.rank:
             return
         self.throw(new=player.new_game)
-        self.moved = False
+        self.moved = 0
         possible = self.n_moves_possible(player)
         if possible == 0:
             player.moves_stalled += 1
@@ -64,20 +69,28 @@ class Clobrdo:
         if not self.moved and possible:
             raise DidNotMove("Player did not move, even though moves are possible", player, self)
 
-        if player.finish == [True] * self.n_pieces:
+        if self.moved > 1:
+            raise MovedMoreThanOnce("Player moved more than once", player, self)
+
+        if player.finish == self.full_house:
             self.rank.append(player)
             self.players.remove(player)
+
+        if self.die == self.n_die:
+            player.new_game = False
+            self.player_move(player)
 
     def throw(self, new=False):
         """Updates die state"""
         if new:
             # 3 throws at the start of the game
             throws = [randint(1, self.n_die) for _ in range(3)]
-            if 6 in throws:
-                self.die = 6
+            if self.n_die in throws:
+                self.die = self.n_die
             else:
                 self.die = throws[2]
-        self.die = randint(1, self.n_die)
+        else:
+            self.die = randint(1, self.n_die)
         self.n_moves += 1
 
     def n_moves_possible(self, player):
@@ -86,109 +99,54 @@ class Clobrdo:
             if self.move_possible(player, piece):
                 moves_possible += 1
             # Check for possible entering
-            if len(player.pieces()) < self.n_pieces and self.die == 6:
+            if len(player.pieces()) < self.n_pieces and self.die == self.n_die:
                 moves_possible += 1
         return moves_possible
+
+    def positions(self, player, field_from):
+        absolute_from = player.unshift_field(field_from)
+
+        from_board = field_from < self.board_size and self.board.get(absolute_from) == player
+        from_finish = field_from >= self.board_size and player.finish[field_from - self.board_size]
+
+        field_to = field_from + self.die
+        absolute_to = player.unshift_field(field_to)
+        finish_border = self.board_size + self.n_pieces
+        to_finish = self.board_size <= field_to < finish_border and not player.finish[field_to - self.board_size]
+        to_board = field_to < self.board_size and self.board.get(absolute_to) != player
+
+        return absolute_from, from_board, from_finish, absolute_to, to_board, to_finish
 
     def move_possible(self, player, field_from):
         """Checks if move from field is valid or not.
         Expects arguments relative to player in question"""
 
-        # Moving piece in game
-        if self.board.get(player.unshift_field(field_from)) == player:
-            field_to = field_from + self.die
-
-            # Destination in finish
-            if self.board_size <= field_to < self.board_size + self.n_pieces:
-                if not player.finish[field_to - self.board_size]:
-                    return True
-                return False
-
-            # In "normal" fields
-            if field_to < self.board_size:
-                # Prevents self-throwing off
-                if self.board.get(player.unshift_field(field_to)) == player:
-                    return False
+        if field_from == START and self.die == self.n_die:
+            if len(player.pieces()) < self.n_pieces and self.board.get(player.start) != player:
                 return True
-            else:
-                return False
 
-        # Piece entering the game
-        if field_from == START and len(player.pieces()) < self.n_pieces and self.die == 6:
+        _, from_board, from_finish, _, to_board, to_finish = self.positions(player, field_from)
+
+        if (from_board or from_finish) and (to_finish or to_board):
             return True
+
         return False
 
     def move(self, player, field_from):
         """Expects arguments relative to player in question"""
         if self.move_possible(player, field_from):
-            if field_from == START:
-                self.board[player.start] = player
-                self.moved = True
-                return
-            field_to = field_from + self.die
-            if field_to >= self.board_size:
-                player.finish[field_to - self.board_size] = True
-            else:
-                self.board[player.unshift_field(field_to)] = player
-            if field_from >= self.board_size:
+            abs_from, from_board, from_finish, abs_to, to_board, to_finish = self.positions(player, field_from)
+
+            if from_board:
+                self.board.pop(abs_from)
+            if from_finish:
                 player.finish[field_from - self.board_size] = False
-            else:
-                self.board.pop(player.unshift_field(field_from))
-            self.moved = True
+            if to_board:
+                self.board[abs_to] = player
+            if to_finish:
+                player.finish[field_from + self.die - self.board_size] = True
+
+            self.moved += 1
+
         else:
             raise IllegalMove("This piece can not make this step or belongs to someone else", player, self)
-
-
-class BasePlayer:
-    def __init__(self, name=None):
-        if name is None:
-            self.name = self.__class__.__name__
-        else:
-            self.name = name
-        self.start = None
-        self.finish = []
-        self.new_game = True
-        self._board = None
-        self.moves_stalled = 0
-
-    @property
-    def n_pieces(self):
-        return self._board.n_pieces
-
-    def strategy(self):
-        """One call of function should result in one move on the board if such move is possible"""
-        pass
-
-    def __repr__(self):
-        return self.__class__.__name__+"(%s)" % self.name
-
-    def __str__(self):
-        return self.__class__.__name__+"(%s)" % self.name
-
-    def die(self):
-        return self._board.die
-
-    def move(self, field_from):
-        self._board.move(self, field_from)
-
-    def pieces(self, target_player=None):
-        """Returns list of all pieces in play belonging to a target_player (if not specified return the pieces of player
-        asking"""
-        # TODO: Should players see to finishes of others?? Hard to implement in current state.
-        if target_player is None:
-            target_player = self
-        pieces = [self.shift_field(item[0]) for item in self._board.board.items() if item[1] == target_player]
-        if target_player == self:
-            pieces += [self._board.board_size+i for i, x in enumerate(self.finish) if x]
-        return pieces
-
-    def enemies(self):
-        return [player for player in self._board.players if player != self]
-
-    def shift_field(self, field):
-        """Player object wraps the playing board in such a way that it is seen by the strategy as indexed in terms of
-        its on start and finish. This is done using shift_field and unshift_field functions."""
-        return (field - self.start) % self._board.board_size
-
-    def unshift_field(self, field):
-        return (field + self.start) % self._board.board_size
